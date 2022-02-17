cfefb4b1cddbcdc5e60e92d1908c50c898269a59
Sidney Henderson <shende39@uncc.edu>
2022-02-17 12:43:59 -0500

    Added more JWT Code

--- src/main/java/com/cognixia/jump/service/MyUserDetailsService.java
+++ src/main/java/com/cognixia/jump/service/MyUserDetailsService.java
@@ -1,24 +1,40 @@
 package com.cognixia.jump.service;
 
-import java.util.ArrayList;
+import java.util.Optional;
 
-import org.springframework.security.core.userdetails.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.cognixia.jump.model.User;
+import com.cognixia.jump.repository.UserRepository;
+
+
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+	@Autowired
+	private UserRepository userRepository;
+	
 	// typically will a user that is pulled from the repository, but we're not going to add that in,
 	// will just hard code a user
+	public UserDetails findByUsername(String username) throws UsernameNotFoundException {
+		
+		
+        Optional<User> user = userRepository.findByUsername(username);
+		return (UserDetails) user.get();
+        
+    }
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-		// no matter what username or password we use/pass in the request, this user with the 
-		// below credentials will always be returned
-		return new User("user1", "pw123", new ArrayList<>());
+		Optional<User> user = userRepository.findByUsername(username);
+		return (UserDetails) user.get();
+	}
 	}
 
-}
+
